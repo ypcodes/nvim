22e5e3a182ec4ac60780f323c1082a9bcc5bb43a
Peng Ye <yemouren@proton.me>
2023-02-26 14:01:15 +0800

    add some keybindings

--- init.lua
+++ init.lua
@@ -1,10 +1,24 @@
+-- Define a global table to store mappings
+_G.mappings = {}
+
+-- Define a function that sets a mapping and adds it to the table
+--[[ local function map(mode, lhs, rhs, opts)
+  local options = {noremap = true}
+  if opts then options = vim.tbl_extend('force', options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+  -- Add the mapping to the table with mode and lhs as keys
+  _G.mappings[mode .. lhs] = {rhs = rhs, opts = options}
+end ]]
+
+local map = vim.api.nvim_set_keymap
+local options= { noremap = true, silent = true }
+
 require('settings')
 require('maps')
 
 -- map
-local map = vim.api.nvim_set_keymap
-local options = { noremap = true }
-local cmd_options = { noremap = true, silent = true }
+-- local map = vim.api.nvim_set_keymap
+-- local options = { noremap = true, silent = true }
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -408,7 +422,12 @@ require("lazy").setup({
 		require('org-bullets').setup()
 	end,},
 	{"dhruvasagar/vim-table-mode"},
-	{'michaelb/sniprun'},
+	{
+		'michaelb/sniprun',
+		config = function()
+			map('v', 'X', ":SnipRun<CR>", options)
+		end,
+	},
 
 	{ 'echasnovski/mini.nvim', version = false },
 	{ 'echasnovski/mini.tabline', version = false , config = function ()
@@ -433,35 +452,6 @@ require("lazy").setup({
 	{"ellisonleao/glow.nvim", config = true, cmd = "Glow"},
 	{ 'echasnovski/mini.map', version = false, config = function() require('mini.map').setup() end, },
 	{ 'echasnovski/mini.starter', version = false, config = function() require('mini.starter').setup() end, },
-	{'akinsho/toggleterm.nvim', version = "*",
-	config = function()
-		function _G.set_terminal_keymaps()
-			local opts = {buffer = 0}
-			vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-			vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-			vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-			vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-			vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-			vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-			vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
-		end
-
-		-- if you only want these mappings for toggle term use term://*toggleterm#* instead
-		vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
-
-		local Terminal  = require('toggleterm.terminal').Terminal
-		local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-
-		function _lazygit_toggle()
-			lazygit:toggle()
-		end
-
-		function _term_toggle()
-			Terminal:new({cmd = "bash", hidden = false})
-		end
-
-		shade_filetypes = { "none", "fzf" }
-	end,},
 	{
 		"nvim-tree/nvim-web-devicons",
 		config = function()
@@ -589,22 +579,22 @@ require("lazy").setup({
 				-- your configuration comes here
 				-- or leave it empty to use the default settings
 				-- refer to the configuration section below
-				vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
+				map("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
 				{silent = true, noremap = true}
 				),
-				vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
+				map("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
 				{silent = true, noremap = true}
 				),
-				vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
+				map("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
 				{silent = true, noremap = true}
 				),
-				vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
+				map("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
 				{silent = true, noremap = true}
 				),
-				vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
+				map("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
 				{silent = true, noremap = true}
 				),
-				vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
+				map("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
 				{silent = true, noremap = true}
 				),
 			}
@@ -908,6 +898,12 @@ end,
 			"nvim-lua/plenary.nvim",
 			"nvim-telescope/telescope.nvim"
 		}
+	},
+	{
+		"b0o/mapx.nvim",
+		config = function()
+			local m = require'mapx'.setup{ global = true, whichkey = true }
+		end,
 	}
 	-- LazyEnd
 })
