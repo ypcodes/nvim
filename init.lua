10617f5da46c93d6c7ff50e4571b6bdafe93245d
Peng Ye <yemouren@pronton.me>
2023-03-01 13:28:44 +0800

    Fix: config nvim-dap-ui correctly

--- init.lua
+++ init.lua
@@ -934,7 +934,23 @@ require("lazy").setup({
 	{
 		"vijaymarupudi/nvim-fzf",
 	},
-	{ "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } },
+	{
+		"rcarriga/nvim-dap-ui",
+		requires = { "mfussenegger/nvim-dap" },
+		config = function()
+			require("dapui").setup({})
+			local dap, dapui = require("dap"), require("dapui")
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
+		end,
+	},
 	{ "liuchengxu/vim-clap" }, -- remenber to run: Clap install-binary
 	-- LazyEnd
 })
